2cfa54bce353f5c5da1a4a6ad6f7b6157043acef
zhongkang <525431447@qq.com>
2018-12-05 19:27:57 +0800

    say 啊

---
+++ julia/src/main/java/com/japan/movie/Julia.java
@@ -0,0 +1,12 @@
+package com.japan.movie;
+
+public class Julia {
+    public static void main(String[] args) {
+
+
+    }
+
+    public void say(){
+        System.out.println("啊，啊，啊，啊，啊");
+    }
+}
